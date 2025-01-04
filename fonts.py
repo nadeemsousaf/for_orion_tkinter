fc83b0bd5db90f7913e9c71be8f8152da9f181db
nadeemsousaf <nmsou6754@gmail.com>
2025-01-03 23:05:01 -0500

    up_to_typing

--- fonts.py
+++ fonts.py
@@ -1,5 +1,7 @@
 fonts = {
     '1' : ("OCR A Extended", 35),
     '2' : ("OCR A Extended", 40),
+    '3' : ("OCR A Extended", 15),
+    '4' : ("OCR A Extended", 25),
 }
 
--- stroll.py
+++ stroll.py
@@ -16,14 +16,47 @@ user = character()
 current_widgets = []
 
 def opening_title():
-    button_1 = Button(window, text="Start", font=fonts['1'], command=prologue)
+    button_1 = Button(window, text="Start", font=fonts['4'], command=loading_bar)
     canvas.create_window(700, 400, anchor="nw", window=button_1)
     canvas.create_text(400, 300, anchor="nw", text="For Orion: A Sci-Fi RPG", font=fonts['2'])
     canvas.pack(fill="both", expand=True)
+
+def loading_bar():
+    clean_canvas()
+    load_bar = ttk.Progressbar(mode="indeterminate")
+    current_widgets.append(load_bar)
+    load_bar.place(x=600, y=400, width=350)
+    load_bar.start()
+    start_prologue = lambda: prologue()
+    window.after(2000, start_prologue)
     
+
 def prologue():
     clean_canvas()
-    ok = 1
+    type_text("Mission Report:", 400, 100, fonts['3'])
+    text_1 = lambda: type_text("Rocket is off course.", 400, 150, fonts['3'])
+    window.after(1000, text_1)
+
+def type_text(text, x, y, text_font):
+    building_text = ""
+    index = 0
+    def typing():
+        canvas.create_text(x, y, anchor="nw", text=building_text, font=text_font)
+        index += 1
+        print(index)
+    try:
+        building_text += text[index]
+        #typing = lambda: canvas.create_text(x, y, anchor="nw", text=building_text, font=text_font); index += 1
+        window.after(1000, typing())
+        '''
+        for char in text:
+            building_text = building_text + char
+            canvas.create_text(x, y, anchor="nw", text=building_text, font=text_font)
+            typing = lambda: canvas.create_text(x, y, anchor="nw", text=building_text, font=text_font)
+            window.after(1000, typing)
+        '''
+    except IndexError:
+        return
 
 def clean_canvas():
     for i in current_widgets:
