00f7cc7541ceadda7db01347e8041ebe64f0572b
nadeemsousaf <nmsou6754@gmail.com>
2025-01-04 19:45:00 -0500

    WE_ARE_TYPING

--- stroll.py
+++ stroll.py
@@ -13,7 +13,7 @@ canvas = Canvas(window, width=400, height=400)
 user = character()
 
 #widget holder
-current_widgets = []
+current_widgets = [] #widgets that do not disappear upon calling "canvas.delete('all')"
 
 def opening_title():
     button_1 = Button(window, text="Start", font=fonts['4'], command=loading_bar)
@@ -21,43 +21,50 @@ def opening_title():
     canvas.create_text(400, 300, anchor="nw", text="For Orion: A Sci-Fi RPG", font=fonts['2'])
     canvas.pack(fill="both", expand=True)
 
-def loading_bar():
+def loading_bar(): #just for graphics
     clean_canvas()
-    load_bar = ttk.Progressbar(mode="indeterminate")
+    load_bar = ttk.Progressbar(mode="determinate")
     current_widgets.append(load_bar)
-    load_bar.place(x=600, y=400, width=350)
+    load_bar.place(x=600, y=380, width=350)
     load_bar.start()
-    start_prologue = lambda: prologue()
-    window.after(2000, start_prologue)
-    
+
+    prologue() #speeding up for now
+
+    #start_prologue = lambda: prologue() #need
+    #window.after(6200, start_prologue) #need
 
 def prologue():
     clean_canvas()
     type_text("Mission Report:", 400, 100, fonts['3'])
-    text_1 = lambda: type_text("Rocket is off course.", 400, 150, fonts['3'])
-    window.after(1000, text_1)
+    body_text_1 = lambda: type_text("Rocket is off course.", 400, 150, fonts['3'])
+    body_text_2 = lambda: type_text("Rocket is off course.", 400, 150, fonts['3'])
+    body_text_3 = lambda: type_text("Rocket is off course.", 400, 150, fonts['3'])
+    body_text_4 = lambda: type_text("Rocket is off course.", 400, 150, fonts['3'])
+    window.after(1000, body_text_1)
+    window.after(2000, body_text_2)
+    window.after(3000, body_text_3)
+    window.after(4000, body_text_4)
+    button_1 = Button(window, text="Continue...", font=fonts['4'], command=sign_off_report)
+    appearing_button = lambda : canvas.create_window(700, 400, anchor="nw", window=button_1)
+    window.after(5000, appearing_button)
 
-def type_text(text, x, y, text_font):
-    building_text = ""
-    index = 0
-    def typing():
-        canvas.create_text(x, y, anchor="nw", text=building_text, font=text_font)
-        index += 1
-        print(index)
-    try:
-        building_text += text[index]
-        #typing = lambda: canvas.create_text(x, y, anchor="nw", text=building_text, font=text_font); index += 1
-        window.after(1000, typing())
-        '''
-        for char in text:
-            building_text = building_text + char
-            canvas.create_text(x, y, anchor="nw", text=building_text, font=text_font)
-            typing = lambda: canvas.create_text(x, y, anchor="nw", text=building_text, font=text_font)
-            window.after(1000, typing)
-        '''
-    except IndexError:
-        return
-
+def type_text(text, x, y, font):
+    typing = lambda char: canvas.create_text(x, y, anchor="nw", text=char, font=font)
+    for char in text:
+        window.update()
+        typing(char)
+        window.after(100)
+        x += 10
+    
+def sign_off_report():
+    clean_canvas()
+    name_entry = Entry(canvas, bd=8, width=80)
+    #name_entry.grid(400,150)
+    current_widgets.append(name_entry)
+    name_entry.pack()
+    name = ""
+    user.name = name
+    
 def clean_canvas():
     for i in current_widgets:
         i.destroy()
